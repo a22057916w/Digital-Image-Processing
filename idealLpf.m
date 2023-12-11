af77808ef6fdcd672b2e60236955ad057894f78b
naitea9527 <a22057916w@gmail.com>
2023-12-11 14:47:38 +0800

    cd size of z, add explanation in comment

--- idealLpf.m
+++ idealLpf.m
@@ -13,7 +13,8 @@ image(flog*255/fm);
 colormap(map), axis('square')
 [x,y]= meshgrid(-256:255,-256:255);
 z = sqrt(x.^2+ y.^2);
-clf= (z<15);
+% clf= (z<15);
+clf = (z < 30); % set z < 30
 subplot(3,2,4)
 image(255*clf);
 colormap(map), axis('square')
@@ -27,3 +28,8 @@ yLena=ifft2(yLenaf);
 subplot(3,2,5)
 image(abs(yLena));
 colormap(map), axis('square')
+
+% If the z goes larger, less high frequency gets filter out, the resulting
+% image become less smooth(blur).
+% If the z goes lower, more high frequency get filter out, the resulting
+% image become more smooth(blur).
